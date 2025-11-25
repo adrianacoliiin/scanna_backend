76657b50a12fa2049b64e05aac8690c828840b7f
adrianacoliiin <adriana.colinmunoz@gmail.com>
2025-11-25 11:10:30 -0600

    fixed imports

--- app/routes/auth.py
+++ app/routes/auth.py
@@ -1,4 +1,4 @@
-from fastapi import APIRouter, HTTPException, status
+from fastapi import APIRouter, HTTPException, status, Depends
 from datetime import datetime, timedelta
 
 from app.db.models import (
